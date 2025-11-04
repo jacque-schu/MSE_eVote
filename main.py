fb511082cb3f7529a3c3ff24bc151d151ad4c463
Fabian Scholl <schollfabiantom@gmail.com>
2025-11-04 21:18:59 +0100

    Install Fast API

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+
+app = FastAPI(title="MSE eVote")
+
+BASE_DIR = Path(__file__).parent
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "title": "MSE eVote",
+            "subtitle": "Einfach abstimmen. Klar auswerten."
+        }
+    )
