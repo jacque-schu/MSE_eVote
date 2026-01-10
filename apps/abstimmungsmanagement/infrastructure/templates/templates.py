cbaca1d6514a17bdfcae85652be175864b3d3c91
LunMar13 <m.starker@gmx.net>
2026-01-10 14:30:16 +0100

    Revert "Ergänzung Testdatei test_templates"
    This reverts commit 6ab8ad0f573406c360c11d3d2c58a604687d38db.

--- apps/abstimmungsmanagement/infrastructure/templates/templates.py
+++ apps/abstimmungsmanagement/infrastructure/templates/templates.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 from fastapi.templating import Jinja2Templates
 
-#Die Datei richtet den Template‑Ordner für die Abstimmungsübersicht ein und stellt den Pfad zum HTML-Template bereit
+#Die Datei richtet den Template‑Ordner für die Abstimmungsübersicht ein
+#und stellt den Pfad zum HTML-Template bereit
 
 BASE_DIR = Path(__file__).resolve().parents[4]
 
@@ -10,3 +11,9 @@ templates_abst = Jinja2Templates(
 )
 
 
+#Test, ob der Pfad zum HTML-Template stimmt/ kann später gelöscht werden
+if __name__ == "__main__":
+    path = BASE_DIR / "ui" / "abstimmung" / "templates"
+    print("Template dir:", path)
+    print("Exists:", path.exists())
+    print("Files:", list(path.glob("*.html")))
