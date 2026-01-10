43181e64f2747b2c89c9566c1cd77fd84b7ed296
LunMar13 <m.starker@gmx.net>
2026-01-10 08:41:30 +0100

    Update abstimmung_auth.py
    Änderung Funktionsdefinition von async def zu def

--- apps/abstimmungsmanagement/infrastructure/auth/abstimmung_auth.py
+++ apps/abstimmungsmanagement/infrastructure/auth/abstimmung_auth.py
@@ -4,7 +4,7 @@ from apps.shared.aspects.auth_aspect import is_token_valid, SECRET_KEY, ALGORITH
 
 #Diese Datei ist dein zentraler Auth-Schutz für ALLE Endpunkte
 
-async def require_login(request: Request):
+def require_login(request: Request):
     token = request.cookies.get('auth_token')
     if not token or not is_token_valid(token):
         raise HTTPException(status_code=401, detail="Login fehlt")
